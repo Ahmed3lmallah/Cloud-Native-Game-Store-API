9fcb4748598a2bbcea8c6108cb70bc90654adbd4
Ahmed ElMallah <ahmed.elmalah@live.com>
2019-08-10 14:01:33 -0400

    Initialized Retail-API Service

---
+++ Retail-API/src/main/java/com/company/RetailAPI/RetailApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.RetailAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailApiApplication.class, args);
+	}
+
+}
---
+++ Retail-API/src/test/java/com/company/RetailAPI/RetailApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.RetailAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RetailApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
