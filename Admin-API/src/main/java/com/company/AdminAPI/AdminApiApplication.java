39491fb79f04677443ccc966c718732322b025b7
Ahmed ElMallah <ahmed.elmalah@live.com>
2019-08-10 14:11:57 -0400

    Initialized Admin-API Service

---
+++ Admin-API/src/main/java/com/company/AdminAPI/AdminApiApplication.java
@@ -0,0 +1,15 @@
+package com.company.AdminAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApiApplication.class, args);
+	}
+
+}
