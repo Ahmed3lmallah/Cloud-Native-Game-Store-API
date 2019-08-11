37ae36d519cda18a267f164180241f6d12c7ae1b
Ahmed ElMallah <ahmed.elmalah@live.com>
2019-08-10 22:53:00 -0400

    Initialized Customer Service

---
+++ customer-service/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+create schema if not exists customer;
+use customer;
+
+create table if not exists customer (
+	customer_id int(11) not null auto_increment primary key,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    street varchar(50) not null,
+    city varchar(50) not null,
+    zip varchar(10) not null,
+    email varchar(75) not null,
+    phone varchar(20) not null
+);
