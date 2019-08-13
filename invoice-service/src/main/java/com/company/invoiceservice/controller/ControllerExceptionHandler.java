b1a7cb535f59e5a5aff1a0b3647c6ff6a12cb153
Ahmed ElMallah <ahmed.elmalah@live.com>
2019-08-13 14:59:41 -0400

    Added Invoice Service Controller Layer

---
+++ invoice-service/src/main/java/com/company/invoiceservice/controller/ControllerExceptionHandler.java
@@ -0,0 +1,84 @@
+package com.company.invoiceservice.controller;
+
+import com.company.invoiceservice.exception.NotFoundException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+@RequestMapping(produces = "application/vnd.error+json")
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<VndErrors> serviceValidationError(MethodArgumentNotValidException e, WebRequest request) {
+        // BindingResult holds the validation errors
+        BindingResult result = e.getBindingResult();
+        // Validation errors are stored in FieldError objects
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        // Translate the FieldErrors to VndErrors
+        List<VndErrors.VndError> vndErrorList = new ArrayList<>();
+        for (FieldError fieldError : fieldErrors) {
+            VndErrors.VndError vndError = new VndErrors.VndError(request.toString(), fieldError.getDefaultMessage());
+            vndErrorList.add(vndError);
+        }
+        // Wrap all of the VndError objects in a VndErrors object
+        VndErrors vndErrors = new VndErrors(vndErrorList);
+        // Create and return the ResponseEntity
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(vndErrors, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<VndErrors> illegalArgumentException(IllegalArgumentException e, WebRequest request) {
+        VndErrors error = new VndErrors(request.toString(), e.getMessage());
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+
+    @ExceptionHandler(value = {NotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<VndErrors> notFoundException(NotFoundException e, WebRequest request) {
+        VndErrors error = new VndErrors(request.toString(), e.getMessage());
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        return responseEntity;
+    }
+
+    @ExceptionHandler(value = {NullPointerException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<VndErrors> nullPointerException(NullPointerException e, WebRequest request) {
+        VndErrors error = new VndErrors(request.toString(), "Item Cannot be found in Database: " + e.getMessage());
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        return responseEntity;
+    }
+
+    @ExceptionHandler(value = {InvalidFormatException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<VndErrors> invalidFormatException(InvalidFormatException e, WebRequest request) {
+        VndErrors error = new VndErrors(request.toString(), "Invalid Format: " + e.getMessage());
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+
+    @ExceptionHandler(value = {JsonParseException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<VndErrors> jsonFormatException(JsonParseException e, WebRequest request) {
+        VndErrors error = new VndErrors(request.toString(), "Json file is not formatted correctly: " + e.getMessage());
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+}
---
+++ invoice-service/src/main/java/com/company/invoiceservice/controller/InvoiceController.java
@@ -0,0 +1,54 @@
+package com.company.invoiceservice.controller;
+
+import com.company.invoiceservice.service.ServiceLayer;
+import com.company.invoiceservice.views.InvoiceViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+@RefreshScope
+@RequestMapping(value = "/invoice")
+public class InvoiceController {
+
+    @Autowired
+    ServiceLayer serviceLayer;
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<InvoiceViewModel> getAllInvoices(){
+        return serviceLayer.findAllInvoices();
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public InvoiceViewModel createInvoice(@RequestBody @Valid InvoiceViewModel invoiceViewModel){
+        return serviceLayer.saveInvoice(invoiceViewModel);
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public InvoiceViewModel getInvoice(@PathVariable int id) {
+        return serviceLayer.findInvoice(id);
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public InvoiceViewModel updateInvoice(@RequestBody @Valid InvoiceViewModel invoiceViewModel, @PathVariable int id) {
+        if(id!=invoiceViewModel.getInvoiceId()){
+            throw new IllegalArgumentException("Invoice ID in path must match with request body!");
+        }
+        return serviceLayer.updateInvoice(invoiceViewModel);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public String deleteInvoice(@PathVariable int id) {
+        return serviceLayer.removeInvoice(id);
+    }
+}
